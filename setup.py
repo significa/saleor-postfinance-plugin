7a18e23358e01a050fd11b3db9d804376cff2909
Guilherme Oliveira <guilherme.1805.oliveira@gmail.com>
2022-09-02 12:05:44 +0100

    setup & publish package ci

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name = "saleor-postfinance-plugin",
     version="0.0.1",
     description="Saleor PostFinance gateway plugin.",
-    packages=['./'],
+    packages=['saleor-postfinance-plugin'],
     install_requires=["postfinancecheckout==3.1.1"],
 )
