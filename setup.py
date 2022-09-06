376a5ee5fd6b4c51b10e92eba3578e10ce277e20
Guilherme Oliveira <guilherme.1805.oliveira@gmail.com>
2022-09-06 17:14:18 +0100

    setup and plugin improvement

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ setup(
     name = "saleor-postfinance-plugin",
     version="0.0.1",
     description="Saleor PostFinance gateway plugin.",
-    packages=['saleor-postfinance-plugin'],
+    packages=['saleor_postfinance_plugin'],
     install_requires=["postfinancecheckout==3.1.1"],
     entry_points={
-        'saleor.plugins': [
-            'saleor-postfinance-plugin = saleor-postfinance-plugin.plugin:PostFinanceGatewayPlugin',
+        "saleor.plugins": [
+            "saleor_postfinance_plugin = saleor_postfinance_plugin.plugin:PostFinanceGatewayPlugin"
         ]
     }
-
 )
